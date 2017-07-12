ddf2e7fd9911de713da1c76e3561b12cd35a49d5
maade7 <martin.ade@gatech.edu>
2017-07-12 14:09:27 -0400

    fixed sticky footer and nav location on reload

--- app/App.js
+++ app/App.js
@@ -7,6 +7,8 @@ ReactDOM.render(routes, document.getElementById("app"));
 
 $(document).ready(function() {
 
+
+// loads materialize functions
     (function($){
         $(function(){
             $('.button-collapse').sideNav();
@@ -15,20 +17,29 @@ $(document).ready(function() {
         }); // end of document ready
     })(jQuery); // end of jQuery name space
 
-
-
+// global vars
     var navId = 1;
     var nextLink = 0;
     var thisLink = 0;
     var fileName;
 
+// turns buttons off if on welcome page
+welcome();
+ function welcome() {
 
+    if (navId === 1){
+        $('.saveBTN').text('continue');
+        $('.list-group-item').hide();
+    }else{
+        $('.saveBTN').text('submit');
+        $('.list-group-item').show();
+    }
+}
 
-
+// submit button triggers next page button in side nav
     $('.saveBTN').on("click", function() {
-        // $('#'+ navId).parent().click();
         document.getElementById(navId).click();
-        console.log("clicked");
+        // console.log("clicked");
 
     });
 
@@ -41,15 +52,30 @@ $(document).ready(function() {
         navId = 1 + parseInt($(self).attr('data'));
         nextLink = $('#'+ navId).attr('href');
         thisLink = $('#'+ (navId - 1)).attr('href')
-        console.log(navId);
-        console.log(nextLink);
+        // console.log(navId);
+        // console.log(nextLink);
         fileName = " " + thisLink.substr(11) + ".pdf";
         $('.saveBTN').attr('href',nextLink);
         $('#downBtn').text(fileName);
+        $('html, body').animate({ scrollTop: 0 }, 'fast');
+        welcome();
     });
 
+    // highlight location on reload
+    reload();
+    function reload() {
+        // $('#'+ navId).parent().addClass("active");
+        var currentPage = window.location.href;
+        var pos = currentPage.indexOf("#");
+        var searchLink = currentPage.substr(pos);
+        var active = document.querySelectorAll("a[href='" + searchLink + "']");
+        $(active).parent().addClass("active");
+        console.log(searchLink);
+        console.log(active);
+        navId = parseInt($(active).attr('id'));
+    }
 
-})
+});
 
 
 
