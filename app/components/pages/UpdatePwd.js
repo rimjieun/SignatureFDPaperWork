9b0c91db45e0f620d93c57760e98fc702d6771b2
Jieun Rim <rim.jieun@gmail.com>
2017-07-15 05:44:09 -0400

    password validator done

--- app/components/pages/UpdatePwd.js
+++ app/components/pages/UpdatePwd.js
@@ -23,9 +23,36 @@ var UpdatePwd = React.createClass({
     };
   },
 
+  componentDidUpdate: function() {
+
+    var newPassword = this.state.newPassword;
+    var confirmPassword = this.state.confirmPassword;
+
+    if (!schema.validate(newPassword)) {
+      document.getElementById("new-pwd").classList.remove("fa-check-circle");
+      document.getElementById("new-pwd").classList.add("fa-times-circle");
+    } else if (schema.validate(newPassword)) {
+      document.getElementById("new-pwd").classList.remove("fa-times-circle");
+      document.getElementById("new-pwd").classList.add("fa-check-circle");
+    } else if (newPassword === "") {
+      document.getElementById("new-pwd").classList.remove("fa-check-circle");
+      document.getElementById("new-pwd").classList.remove("fa-times-circle");
+    }
+
+    if (confirmPassword === "") {
+      document.getElementById("conf-pwd").classList.remove("fa-times-circle");
+      document.getElementById("conf-pwd").classList.remove("fa-check-circle");
+    } else if (newPassword !== confirmPassword && confirmPassword !== "") {
+      document.getElementById("conf-pwd").classList.remove("fa-check-circle");
+      document.getElementById("conf-pwd").classList.add("fa-times-circle");
+    } else if (newPassword === confirmPassword && confirmPassword !== "") {
+      document.getElementById("conf-pwd").classList.remove("fa-times-circle");
+      document.getElementById("conf-pwd").classList.add("fa-check-circle");
+    }
+  },
+
   handleChange: function(e) {
     this.setState({[e.target.name]: e.target.value });
-    console.log("poop");
   },
 
   handleSubmit: function(e) {
@@ -45,16 +72,7 @@ var UpdatePwd = React.createClass({
             location.href = nextLocation;
           }
         });
-      } else {
-        console.log("Password must be at least 8 characters long and must contain the following:");
-        console.log("- At least one lowercase letter.");
-        console.log("- At least one uppercase letter.");
-        console.log("- At least one numerical digit.");
-        console.log("- At least one special character.");
-        console.log("- No spaces.");
       }
-    } else {
-      console.log("Passwords do not match.");
     }
   },
 
@@ -65,18 +83,28 @@ var UpdatePwd = React.createClass({
           <div className="row"></div>
           <div className="row">
             <form className="col s12">
+              <div style={{fontSize: "12px", color: "#564648"}}>
+                Password must be at least <span style={{fontWeight: "800"}}>8 characters</span> long and must contain:<br />
+                - At least one lowercase letter<br />
+                - At least one uppercase letter<br />
+                - At least one number<br />
+                - At least one special character<br />
+                - No spaces
+              </div>
               <div className="row">
-                <div className="input-field col s12">
+                <div className="input-field col s12 createPwd">
                   <input id="passwordNew" type="password" className="validate"
                   name="newPassword" onChange={this.handleChange} />
                   <label htmlFor="password">Create New Password</label>
+                  <i id="new-pwd" className="fa pwdIcon" aria-hidden="true"></i>
                 </div>
               </div>
               <div className="row">
-                <div className="input-field col s12">
+                <div className="input-field col s12 createPwd">
                   <input id="passwordNewConfirm" type="password" className="validate"
                    name="confirmPassword" onChange={this.handleChange}/>
                   <label htmlFor="password">Confirm New Password</label>
+                  <i id="conf-pwd" className="fa pwdIcon" aria-hidden="true"></i>
                 </div>
               </div>
               <div className="row">
